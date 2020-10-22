efe87e220656232e7c0f59ff81ca80831ded06f0
aksh-ai <akshayakki6899@gmail.com>
2020-10-22 16:10:48 +0530

    Updated Zero Shot Inference Module

--- tamil_tech/zero_shot.py
+++ tamil_tech/zero_shot.py
@@ -1,142 +1,193 @@
 import os
+import warnings
+
+warnings.filterwarnings("ignore")
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import io
 import torch
 import shutil
+import librosa
 import torchvision
 import torch.nn as nn
+import soundfile as sf
 import torch.nn.functional as F
 from tamil_tech.torch.utils import *
 from tamil_tech.torch.models import *
 
-import librosa
-import soundfile as sf
 import tensorflow as tf
-from tamil_tech.tf.utils.loaders import load_conformer_model
+from tamil_tech.tf.utils import TFSpeechFeaturizer, CharFeaturizer, preprocess_paths, check_key_in_dict, append_default_keys_dict, CONFORMER_L, CONFORMER_M, CONFORMER_S, load_conformer_model, download_file_from_google_drive
+from tamil_tech.tf.models import Conformer
+
+class ConformerTamilASRLite(object):
+    def __init__(self, greedy=False, path='conformer_s_v1.tflite'):
+        self.greedy = greedy
+
+        if os.path.exists(path):
+          pass
+        else:
+          if self.greedy:
+            print("Downloading Model...")
+            file_id = '1GmBgu_q0nU_Q_borNsYs5b_1l2FXzVW3'
+            download_file_from_google_drive(file_id, path)
+            print("Downloaded Model Successfully...")
+          else:
+            print("Downloading Model...")
+            file_id = '1GmBgu_q0nU_Q_borNsYs5b_1l2FXzVW3'
+            download_file_from_google_drive(file_id, path)
+            print("Downloaded Model Successfully...")
+        
+        self.model = tf.lite.Interpreter(model_path=path)
+
+        self.input_details = self.model.get_input_details()
+        self.output_details = self.model.get_output_details()
+
+        self.blank = 0
+        self.dim = 320
+
+        print("Loaded Model...!")
+
+    def read_raw_audio(self, audio, sample_rate=16000):
+        if isinstance(audio, str):
+            wave, _ = librosa.load(os.path.expanduser(audio), sr=sample_rate)
+        elif isinstance(audio, bytes):
+            wave, sr = sf.read(io.BytesIO(audio))
+            wave = np.asfortranarray(wave)
+            if sr != sample_rate:
+                wave = librosa.resample(wave, sr, sample_rate)
+        elif isinstance(audio, np.ndarray):
+            return audio
+        else:
+            raise ValueError("input audio must be either a path or bytes")
+        return wave
+
+    def infer(self, path):
+        signal = self.read_raw_audio(path)
+        self.model.resize_tensor_input(self.input_details[0]["index"], signal.shape)
+        self.model.allocate_tensors()
+        self.model.set_tensor(self.input_details[0]["index"], signal)
+        self.model.set_tensor(
+            self.input_details[1]["index"],
+            tf.constant(self.blank, dtype=tf.int32)
+        )
+        self.model.set_tensor(
+            self.input_details[2]["index"],
+            tf.zeros([1, 2, 1, self.dim], dtype=tf.float32)
+        )
+        self.model.invoke()
+        hyp = self.model.get_tensor(self.output_details[0]["index"])
+        print("".join([chr(u) for u in hyp]), end=' ')
+    
+    def infer_dir(self, directory=None):
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+              print(f"Filename: {file} | ", end='')
+              self.infer(os.path.join(root, file))
+              print('')
 
 class ConformerTamilASR(object):
-  # bug - segmentation fault during TFLite inference
-  def __init__(self, greedy=False, use_gpu=False, tflite=True, conformer_type='M'):
-    if use_gpu == False:
-      try:
-          # Disable all GPUS
-          tf.config.set_visible_devices([], 'GPU')
-          visible_devices = tf.config.get_visible_devices()
-          for device in visible_devices:
-              assert device.device_type != 'GPU'
-          os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-      except:
-          # Invalid device or cannot modify virtual devices once initialized.
+    def __init__(self, greedy=False, path='conformer_s_v1.h5'):
+        config = CONFORMER_S
+
+        speech_featurizer = TFSpeechFeaturizer(config["speech_config"])
+        text_featurizer = CharFeaturizer(config["decoder_config"])
+
+        if os.path.exists(path):
           pass
-          
-    self.tflite = tflite
-    self.greedy = greedy
+        else:
+          print("Downloading Model...")
+          file_id = '1Iy6uwYvMo5KvwbUv4L4EMNFasepGpDmf'
+          download_file_from_google_drive(file_id, path)
+          print("Downloaded Model Successfully...")
+        
+        self.model = Conformer(
+            vocabulary_size=text_featurizer.num_classes,
+            **config["model_config"]
+        )
+        self.model._build(speech_featurizer.shape)
+        self.model.load_weights(path, by_name=True)
+        self.model.add_featurizers(speech_featurizer, text_featurizer)
 
-    self.model, self.state_size, self.blank = load_conformer_model(tflite=self.tflite, greedy=self.greedy, conformer_type=conformer_type)
+        self.greedy = greedy
 
-    if self.model:
-      print("Loaded Model!")
-
-    if self.tflite:
-      self.input_details = self.model.get_input_details()
-      self.output_details = self.model.get_output_details()
-  
-  def read_raw_audio(self, audio, sample_rate=16000):
-    if isinstance(audio, str):
-        wave, _ = librosa.load(os.path.expanduser(audio), sr=sample_rate)
-    elif isinstance(audio, bytes):
-        wave, sr = sf.read(io.BytesIO(audio))
-        wave = np.asfortranarray(wave)
-        if sr != sample_rate:
-            wave = librosa.resample(wave, sr, sample_rate)
-    elif isinstance(audio, np.ndarray):
-        return audio
-    else:
-        raise ValueError("input audio must be either a path or bytes")
-    return wave
+        print("Loaded Model...!")
     
-  def infer_file(self, audio, sample_rate=16000):
-    signal = self.read_raw_audio(audio=audio, sample_rate=sample_rate)
+    def read_raw_audio(self, audio, sample_rate=16000):
+        if isinstance(audio, str):
+            wave, _ = librosa.load(os.path.expanduser(audio), sr=sample_rate)
+        elif isinstance(audio, bytes):
+            wave, sr = sf.read(io.BytesIO(audio))
+            wave = np.asfortranarray(wave)
+            if sr != sample_rate:
+                wave = librosa.resample(wave, sr, sample_rate)
+        elif isinstance(audio, np.ndarray):
+            return audio
+        else:
+            raise ValueError("input audio must be either a path or bytes")
+        return wave
 
-    if self.tflite:
-      self.model.resize_tensor_input(self.input_details[0]["index"], signal.shape)
-      self.model.allocate_tensors()
-      self.model.set_tensor(self.input_details[0]["index"], signal)
-      
-      self.model.set_tensor(
-          self.input_details[1]["index"],
-          tf.constant(self.blank, dtype=tf.int32)
-      )
-      
-      self.model.set_tensor(
-          self.input_details[2]["index"],
-          tf.zeros([1, 2, 1, self.state_size], dtype=tf.float32)
-      )
-      
-      _ = self.model.invoke()
-      
-      predictions = self.model.get_tensor(self.output_details[0]["index"])
-      
-      print("".join([chr(u) for u in predictions]))
-    
-    else:
-      signal = np.expand_dims(self.model.speech_featurizer.extract(signal), axis=1)
-      if self.greedy:
-        pred = self.model.recognize(signal)
-      
-      if self.model.text_featurizer.decoder_config["beam_width"] > 0 and not self.greedy:
+    def bytes_to_string(self, array: np.ndarray, encoding: str = "utf-8"):
+        return [transcript.decode(encoding) for transcript in array]
+
+    def infer(self, path):
+        signal = self.read_raw_audio(path)
+        signal = tf.expand_dims(self.model.speech_featurizer.tf_extract(signal), axis=0)
+        if self.greedy:
+          pred = self.model.recognize(features=signal)
+        else:
           pred = self.model.recognize_beam(features=signal, lm=True)
-      else:
-          pred = beam_lm_pred = tf.constant([""], dtype=tf.string)
 
-      print(tf.convert_to_tensor([pred], dtype=tf.string))
-  
-  def infer_dir(self, directory=None):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-          self.infer_file(os.path.join(root, file))
-  
-  def record_and_stream(self):
-    print("Creating Audio stream...\nSpeak!")
+        print(self.bytes_to_string(pred.numpy())[0], end=' ')
+    
+    def infer_dir(self, directory=None):
+      for root, dirs, files in os.walk(directory):
+          for file in files:
+            print(f"Filename: {file} | ", end='')
+            self.infer(os.path.join(root, file))
+            print('')
+    
+    def record_and_stream(self):
+        print("Creating Audio stream...\nSpeak!")
 
-    self.p = pyaudio.PyAudio()
+        self.p = pyaudio.PyAudio()
 
-    self.stream = self.p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=16000,
-                    input=True,
-                    frames_per_buffer=1024)
-         
-    while True:
-      try:
-        transcription = ""
-        audios = []
-        frames = []
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=16000,
+                        input=True,
+                        frames_per_buffer=1024)
+            
+        while True:
+            try:
+                transcription = ""
+                audios = []
+                frames = []
 
-        for i in range(0, int(16000 / 1024 * 6)):
-          data = self.stream.read(1024)
-          frames.append(data)
-                  
-        wf = wave.open(f'tamil_tech/__pycache__/{self.cnt}.wav', 'wb')
-        wf.setnchannels(1)
-        wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
-        wf.setframerate(16000)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+                for i in range(0, int(16000 / 1024 * 7)):
+                    data = self.stream.read(1024)
+                    frames.append(data)
+                        
+                wf = wave.open(f'tamil_tech/__pycache__/0.wav', 'wb')
+                wf.setnchannels(1)
+                wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
+                wf.setframerate(16000)
+                wf.writeframes(b''.join(frames))
+                wf.close()
 
-        self.infer_file(f'tamil_tech/__pycache__/{self.cnt}.wav')
+                self.infer(f'tamil_tech/__pycache__/0.wav')
 
-      except Exception as e:
-        print(e)
-        break
+            except Exception as e:
+                print(e)
+                break
 
-      except KeyboardInterrupt:
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
-        break
-  
-  def __call__(self):
-    self.record_and_stream()
+            except KeyboardInterrupt:
+                self.stream.stop_stream()
+                self.stream.close()
+                self.p.terminate()
+                break
+    
+    def __call__(self):
+        self.record_and_stream()
 
 class DeepTamilASR(object):
   def __init__(self, use_gpu=True):
